e79eb3c6ed9822ff964445ac49a05d305f52c30f
james-rossano <jrossano4@gmail.com>
2020-02-29 08:42:08 -0500

    Changed program to class but still many bug fixes to address in pynput

--- readFast.py
+++ readFast.py
@@ -1,52 +1,65 @@
 import pygame, time
 from pynput import keyboard
-pygame.init()	
+pygame.init()
 x,y = 500,500
-WPM = 200
-delay = 1/(WPM/60)
-spacePress = False
 
 def pressed(key):
-	global spacePress
+	print('pressed')
 	if str(key) == 'Key.space':
 		if spacePress == True:
 			spacePress = False
-		else:
+			return spacePress 
+		else: 
 			spacePress = True
+			return spacePress
 
 def rotate(Givenlist, shift):
-	return Givenlist[shift:] + Givenlist[:shift]
+		return Givenlist[shift:] + Givenlist[:shift]
 
-display_surface = pygame.display.set_mode((x, y))
-pygame.display.set_caption('Read Lines') 
-font = pygame.font.Font('freesansbold.ttf', 50) 
-book = open("BraveNewWorld.txt","r",encoding="utf8") 
-bookLines = book.readlines()
-running = True
-wordRemain = ['1','2','3','4','5']
-yValues = [400,300,200,100]	
+class readText:
+	def __init__(self,textFile,WPM,fontSize=None):
+		self.fontSize = fontSize or 50
+		self.textFile = textFile
+		self.WPM = WPM
+		self.delay = 1/(self.WPM/60)
 
-listener = keyboard.Listener(on_press=pressed)
-listener.start()
-for line in bookLines:
-	stringSplit = line.split()
-	for i in range(len(stringSplit)):
-		if spacePress == False:
-			wordRemain = rotate(wordRemain,-1)
-			wordRemain[0] = stringSplit[i]
-			display_surface.fill((255,255,255)) 
-			for g in range(len(yValues)):
-				text = font.render(wordRemain[g-1], True, (0,0,0), (255,255,255))
-				textRect = text.get_rect() 
-				textRect.center = (x/2,yValues[g-1])
-				display_surface.blit(text, textRect) 
-			pygame.display.flip()
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
-		if running == False:
-			break	
-		time.sleep(delay)
-	if running == False:
-		break
-book.close()
+	def readText(self):
+		spacePress = False
+		text = open(self.textFile,"r",encoding="utf8")
+		font = pygame.font.Font('freesansbold.ttf', self.fontSize) 
+		display_surface = pygame.display.set_mode((x, y))
+		pygame.display.set_caption('textFile') 
+		textLines = text.readlines()
+		running = True
+		wordRemain = ['1','2','3','4','5']
+		yValues = [400,300,200,100]	
+
+		listener = keyboard.Listener(on_press=pressed)
+		listener.start()
+
+		for line in textLines:
+			stringSplit = line.split()
+			for i in range(len(stringSplit)):
+				if spacePress == False:
+					wordRemain = rotate(wordRemain,-1)
+					wordRemain[0] = stringSplit[i]
+					display_surface.fill((255,255,255)) 
+					for g in range(len(yValues)):
+						textfont = font.render(wordRemain[g-1], True, (0,0,0), (255,255,255))
+						textRect = textfont.get_rect() 
+						textRect.center = (x/2,yValues[g-1])
+						display_surface.blit(textfont, textRect) 
+					pygame.display.flip()
+				for event in pygame.event.get():
+					if event.type == pygame.QUIT:
+						running = False
+				if running == False:
+					break	
+				time.sleep(self.delay)
+			if running == False:
+				break
+		text.close()
+
+filePath = 'C:/Users/James Rossano/Documents/My_Shit/Programming_Files/BraveNewWorld.txt'
+file = readText(filePath,200,40)
+file.readText()
